f2f4b935c5583995f28e4685023d8d50e3ee150e
psjayasurya <psjayasurya2003@gmail.com>
2026-01-07 17:58:10 +0530

    Add files via upload

---
+++ static/data.js
@@ -0,0 +1,885 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrance",
+      "name": "Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.187099663540355,
+        "pitch": -0.013814975452785205,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9297215742373357,
+          "pitch": 0.08582922568214713,
+          "rotation": 1.5707963267948966,
+          "target": "8-store-1"
+        },
+        {
+          "yaw": -1.3511961542842492,
+          "pitch": 0.49001470555519155,
+          "rotation": 0,
+          "target": "1-entrance-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-entrance-2",
+      "name": "Entrance 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.09470913183315588,
+        "pitch": 0.0646469484782699,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8130225284218326,
+          "pitch": 0.4178099057140052,
+          "rotation": 0,
+          "target": "12-inside-1"
+        },
+        {
+          "yaw": -1.9705451829701879,
+          "pitch": 0.4187033354357226,
+          "rotation": 0,
+          "target": "15-stairs-2"
+        },
+        {
+          "yaw": -0.8699956896516845,
+          "pitch": 0.23347141557203877,
+          "rotation": 4.71238898038469,
+          "target": "8-store-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-1st-floor-1",
+      "name": "1st floor 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.08230890723573481,
+        "pitch": 0.01835892293892094,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0112472274381323,
+          "pitch": 0.4666912818327553,
+          "rotation": 0,
+          "target": "17-stairs-4"
+        },
+        {
+          "yaw": -3.00983651664896,
+          "pitch": 0.2620241833881991,
+          "rotation": 6.283185307179586,
+          "target": "3-1st-floor-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.17528398573452897,
+          "pitch": 1.132519801430048,
+          "title": "Menu",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-1st-floor-2",
+      "name": "1st floor 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.3957928274525404,
+        "pitch": -0.07242073014135109,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.02694247630106794,
+          "pitch": 0.1606674545325255,
+          "rotation": 0,
+          "target": "2-1st-floor-1"
+        },
+        {
+          "yaw": -2.886417033593318,
+          "pitch": 0.1313474054493753,
+          "rotation": 1.5707963267948966,
+          "target": "4-1st-floor-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.800681870995378,
+          "pitch": 0.5743162915028215,
+          "title": "Menu",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-1st-floor-3",
+      "name": "1st floor 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.722134237496153,
+        "pitch": 0.1745410380935546,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.48904959962050754,
+          "pitch": 0.2370093032431324,
+          "rotation": 1.5707963267948966,
+          "target": "3-1st-floor-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9433115183863592,
+          "pitch": 1.0684611305466944,
+          "title": "Menu",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-store-4",
+      "name": "store 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.39373708589571,
+        "pitch": 0.2769836524787479,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.111755583114869,
+          "pitch": 0.46521326664339213,
+          "rotation": 0,
+          "target": "9-store-5"
+        },
+        {
+          "yaw": 1.5451649687559996,
+          "pitch": 0.5799731141416515,
+          "rotation": 0,
+          "target": "6-store-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-store-3",
+      "name": "store 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.6818749896176115,
+        "pitch": 0.1369788748700742,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5696624813050821,
+          "pitch": 0.8822698376215197,
+          "rotation": 0,
+          "target": "7-store-2"
+        },
+        {
+          "yaw": -1.17836362087208,
+          "pitch": 0.5675142910254856,
+          "rotation": 0,
+          "target": "5-store-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-store-2",
+      "name": "store 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.4350705845332357,
+        "pitch": 0.06898023602098924,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5620500057181097,
+          "pitch": 0.5241196498781484,
+          "rotation": 0,
+          "target": "8-store-1"
+        },
+        {
+          "yaw": -1.262337870363801,
+          "pitch": 0.5602773256791487,
+          "rotation": 0,
+          "target": "6-store-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-store-1",
+      "name": "store 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.91886783541079,
+        "pitch": 0.113572598332631,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4694540074315725,
+          "pitch": 0.5730106919400964,
+          "rotation": 0,
+          "target": "7-store-2"
+        },
+        {
+          "yaw": 1.678061725009088,
+          "pitch": 0.29606429288421765,
+          "rotation": 4.71238898038469,
+          "target": "0-entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-store-5",
+      "name": "store 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.1122764335798703,
+        "pitch": 0.05751328549096968,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1250892523761244,
+          "pitch": 0.46648092947838293,
+          "rotation": 0,
+          "target": "5-store-4"
+        },
+        {
+          "yaw": 2.1780778755632673,
+          "pitch": 0.4732722130427547,
+          "rotation": 0,
+          "target": "10-store-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-store-6",
+      "name": "store 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.1314680756449409,
+        "pitch": 0.13539126950957758,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8939815473890285,
+          "pitch": 0.5150711842911502,
+          "rotation": 0,
+          "target": "9-store-5"
+        },
+        {
+          "yaw": -2.0141301155384355,
+          "pitch": 0.5723854769377539,
+          "rotation": 0,
+          "target": "11-store-7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-store-7",
+      "name": "store 7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.810918681744587,
+        "pitch": -0.04517979901010705,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.960773772073047,
+          "pitch": 0.6253708158527029,
+          "rotation": 0,
+          "target": "0-entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-inside-1",
+      "name": "inside 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.7217950635638477,
+        "pitch": -0.36289651676263723,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.040990879965266,
+          "pitch": 0.002958543699108418,
+          "rotation": 3.141592653589793,
+          "target": "13-inside-center"
+        },
+        {
+          "yaw": -0.07231616605412938,
+          "pitch": -0.050459228276066526,
+          "rotation": 0,
+          "target": "1-entrance-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.7186334794253026,
+          "pitch": 0.1592051672247905,
+          "title": "Menu",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "13-inside-center",
+      "name": "inside center",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.00598866587437108,
+        "pitch": -0.2784973544023117,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.23488404326926116,
+          "pitch": 0.10199664276778009,
+          "rotation": 0,
+          "target": "1-entrance-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.025762621256443907,
+          "pitch": 0.3480348072589443,
+          "title": "Menu",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "14-stairs-1",
+      "name": "Stairs 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-stairs-2",
+      "name": "stairs 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.4993994064928522,
+        "pitch": 0.29835103352990444,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7275243967653537,
+          "pitch": 0.968253307155015,
+          "rotation": 0,
+          "target": "16-stairs-3"
+        },
+        {
+          "yaw": -0.5179768451144238,
+          "pitch": 0.8077946339221036,
+          "rotation": 0,
+          "target": "1-entrance-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-stairs-3",
+      "name": "stairs 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.195637090225679,
+        "pitch": 0.013648843103139185,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1020715588848935,
+          "pitch": 0.9930444611126621,
+          "rotation": 1.5707963267948966,
+          "target": "15-stairs-2"
+        },
+        {
+          "yaw": -2.5236326097824247,
+          "pitch": -0.008972103063305426,
+          "rotation": 0,
+          "target": "17-stairs-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "17-stairs-4",
+      "name": "stairs 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.4304357647317314,
+        "pitch": 0.044769633202124126,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8072140139162505,
+          "pitch": 0.6751573209776129,
+          "rotation": 0,
+          "target": "16-stairs-3"
+        },
+        {
+          "yaw": 1.247055794583499,
+          "pitch": 0.66322032784608,
+          "rotation": 0,
+          "target": "2-1st-floor-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
